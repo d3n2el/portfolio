5004af977f05b57652adcfe4422d522f7a249891
Daniel Lialin <118548099+d3n2el@users.noreply.github.com>
2025-06-17 09:56:34 +0200

    Add files via upload

---
+++ game.js
@@ -0,0 +1,13 @@
+// Setup game canvas in js
+
+    //Create simple rectangle to test with
+
+    // Make it movabl with user input
+        // Change velocity of a character
+        // Use x and y coordinates to help with movement
+        // Once it starts to move, make sure to remove the rectangle
+        //from its starting place so that it doesn't just become a straight line
+        // setup canvas limits so that it doesn't disappear into nothing
+        // Make sure that everything works and test it 100 times
+        
+
--- main.js
+++ main.js
@@ -1,4 +1,7 @@
-window.alert("hello world")
-document.getElementById("hello1").textContent = "Hello world" ;
-document.getElementById
-// this is a comment, ok this works. Perfect
+//What do i need to do in this file?
+//if yes button is clicked, redirect to game page
+button onclick= "document.getElementById('YesOption')"
+button onclick= "document.getElementById('NoOption')
+    // else, redirect to alt page
+    //then everything else should have it's own js  and css file)
+    
