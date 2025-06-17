0e588f6276698928b65dd731970ad52b6f1157f7
Daniel Lialin <118548099+d3n2el@users.noreply.github.com>
2025-06-17 15:51:57 +0200

    Add files via upload

--- InputHandler.js
+++ InputHandler.js
@@ -1,11 +1,43 @@
 
 addEventListener("keydown", function(k){
-    if (k.code == "RightArrow"){
+    console.log(k.code)
+    if (k.code == "ArrowRight"){
         vx = 5;
-        }
-})
+}})
 addEventListener("keyup" , function(k){
-    if(k.code == "RightArrow"){
+    if(k.code == "ArrowRight"){
         vx= 0
     }
 })
+addEventListener("keydown", function(k){
+    if(k.code == "ArrowLeft")
+        vx = -5;
+})
+addEventListener("keyup", function(k){
+    if(k.code == "ArrowLeft")
+        vx = 0;
+})
+addEventListener("keydown" , function(k){
+    if(k.code == "Space"){
+        vy = 5;
+    }
+})
+addEventListener("keyup", function(k){
+    if(k.code == "Space"){
+        vy=0
+    }
+})
+//Need to implement gravity to make the rectangle get back down
+    // gravity: if  object is at an y > 0, decrease the y till it reaches 0?
+//if(y >0){
+    //use a for loop to lower y one at a time
+    //for(let i =0;i < y ; i++){
+        //y--
+        //noticed that the rectangle still stays the same way, need to rectClear the thing one step at a time
+
+    //}
+
+//}
+//everything works except for space, debugging. I'm desperate, it works at the start and then it just creates a straight line
+//half fixed, the problem is that it also blocks all other movement if not for space when it starts, do not understand that
+// it works with the space but i dont understand why not with the arrows, need to solve(noticed just now)
--- game.js
+++ game.js
@@ -7,6 +7,7 @@ let y = 0;
 let vx =  0;
 let vy = 0;
 function update(){
+    ctx.clearRect(0,0,0,0)
     ctx.clearRect(0,0,Canvas.height, Canvas.width)
     x += vx
     y += vy;
@@ -14,12 +15,14 @@ function update(){
     requestAnimationFrame(update)
 }
 update()
-    // Make it movabl with user input(decided at the end to use other file for that)
-        // Change velocity of a character
-        // Use x and y coordinates to help with movement
-        // Once it starts to move, make sure to remove the rectangle
-        //from its starting place so that it doesn't just become a straight line
+    // Make it movabl with user input(decided at the end to use other file for that) x
+        // Change velocity of a character x 
+        // Use x and y coordinates to help with movement x 
+        // Once it starts to move, make sure to remove the rectangle x
+        //from its starting place so that it doesn't just become a straight line x
         // setup canvas limits so that it doesn't disappear into nothing
         // Make sure that everything works and test it 100 times
+        // it doesn't work and i dont understand why
+        // Reminder for self: cannot get favicon, dont even know what is that atm
         
 
