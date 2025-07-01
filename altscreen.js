d3873a0ba353ad7b89b1767f7165bcb9cf281b9e
Daniel Lialin <118548099+d3n2el@users.noreply.github.com>
2025-07-01 16:21:39 +0200

    Add files via upload

--- altscreen.js
+++ altscreen.js
@@ -1,10 +1,11 @@
 const ExploreButton = document.getElementById("explore-button")
 const prevButton = document.getElementById("prevButton")
 const nextButton = document.getElementById("nextButton")
-const slidesOverlay = document.getElementById("slides-overlay")
-const closeOverlayButton = document.getElementById("close-button")
+const slidesOverlay = document.getElementById("slidesOverlay")
+const closeOverlayButton = document.getElementById("closeOverlayButton")
 const funButton = document.getElementById("fun-button")
 const mainName = document.querySelector(".main-name")
+const starButton = document.querySelector(".star-button")
 let currentSlideIndex = 0
 let isAnimating = false // avoid multiple animations later on
 // check when button is clicked
@@ -14,10 +15,9 @@ let isAnimating = false // avoid multiple animations later on
 // should also generate the dots to keep track, will do this last since I might wanna change this
 funButton.addEventListener('click',() =>{
   funButton.classList.toggle('active');
-
-  if(this.classList.contains('active')){
-    mainName.style
-  }
+  mainName.classList.toggle('active'); 
+  ExploreButton.classList.toggle('active');
+  starButton.classList.toggle('active');
 })
 ExploreButton.addEventListener('click', () => {
     slidesOverlay.classList.add('visible'); // Make the overlay visible via CSS class.
@@ -25,7 +25,19 @@ ExploreButton.addEventListener('click', () => {
     showSlide(currentSlideIndex); // will display current slide, need to code the function later
     generateDots(); // Generate the dot indicators
 });
+closeOverlayButton.addEventListener('click', () => {
+    slidesOverlay.classList.remove('visible');
+});
 // i will make comments and pseudocode as per usual to guide me through what i need to do
 function showSlide(currentSlideIndex){
-  //when this function is caled, I need to make appear the slides items at current index
+  //when this function is caled, I need to make appear the slides items at current 
+    const slides = document.querySelectorAll('.slide-item')
+    // hide all slides initially
+    slides.forEach(slide => {
+        slide.classList.remove('active');
+    });
+    // Show current slide if it exists
+    if (slides[currentSlideIndex]) {
+        slides[currentSlideIndex].classList.add('active');
+    }
 }
